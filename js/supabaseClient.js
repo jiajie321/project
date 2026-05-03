a75c7887b33cb55aed64d7b8c9d86353a90f420d
jiajie321 <tanjiajie0321@e.neivce.edu.my>
2026-05-03 15:45:50 +0800

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -2,7 +2,7 @@
 // SUPABASE_URL should look like: https://xxxxx.supabase.co
 // Do NOT add /rest/v1 at the end.
 // Use the publishable key. Do NOT use the secret key in front-end code.
-const SUPABASE_URL = "YOUR_SUPABASE_PROJECT_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_PUBLISHABLE_KEY";
+const SUPABASE_URL = "https://actformcdgxdcmzpvqxg.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_fTFNFB3Aq4_bIIAVGOJA3g_L6p0f2HU";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
