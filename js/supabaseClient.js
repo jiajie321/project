505ff15b024b732d0b630d9bc98153aa42738370
jiajie321 <tanjiajie0321@e.neivce.edu.my>
2026-05-03 15:20:43 +0800

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,5 @@
 // Replace these two values with your Supabase Project URL and anon public key.
-const SUPABASE_URL = "https://actformcdgxdcmzpvqxg.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://actformcdgxdcmzpvqxg.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_fTFNFB3Aq4_bIIAVGOJA3g_L6p0f2HU";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
